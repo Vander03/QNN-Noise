a7c589557b888cccf65c39c486f6ec786f13b14e
Vander03 <schalkvdmerwe232@gmail.com>
2025-10-16 23:55:21 +1000

    added qiskit login script

---
+++ VQC/iris/VQC_classifier_mod/save_qiskit.py
@@ -0,0 +1,8 @@
+from qiskit_ibm_runtime import QiskitRuntimeService
+ 
+QiskitRuntimeService.save_account(
+  token="", # 
+  name="vander", # Optional
+  set_as_default=True, # Optional
+  overwrite=True, # Optional
+)
