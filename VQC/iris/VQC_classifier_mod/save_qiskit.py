b84fcaeaff441d1fcaa3e4441b1181f04a07a4ab
Vander03 <schalkvdmerwe232@gmail.com>
2025-10-17 00:02:54 +1000

    env test

--- VQC/iris/VQC_classifier_mod/save_qiskit.py
+++ VQC/iris/VQC_classifier_mod/save_qiskit.py
@@ -1,7 +1,7 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
  
 QiskitRuntimeService.save_account(
-  token="", # 
+  token="", # Use the 44-character API_KEY you created and saved from the IBM Quantum Platform Home dashboard
   name="vander", # Optional
   set_as_default=True, # Optional
   overwrite=True, # Optional
